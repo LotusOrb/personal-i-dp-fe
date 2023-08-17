62e63cecb35b236e1a4c726c489152f8a8016fe7
Randa <working.rsyafridamara@gmail.com>
2023-08-18 01:16:34 +0700

    feat: add abstraction for http and add lodash

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string;
   readonly VITE_APP_NAME: string;
+  readonly VITE_APP_AUTH_STORAGE: string;
   readonly VITE_APP_MICROSERVICE_DEFAULT: string;
 }
 
